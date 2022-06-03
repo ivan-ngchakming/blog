fa79bcc6579ca1cd349ec5ef2563467806b8b86a
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-06-03 20:57:40 +0800

    feat: added blog post

---
+++ x/blog/keeper/msg_server_create_post.go
@@ -0,0 +1,21 @@
+package keeper
+
+import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/ivan-ngchakming/blog/x/blog/types"
+)
+
+func (k msgServer) CreatePost(goCtx context.Context, msg *types.MsgCreatePost) (*types.MsgCreatePostResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	var post = types.Post{
+		Creator: msg.Creator,
+		Title:   msg.Title,
+		Body:    msg.Body,
+	}
+	id := k.AppendPost(ctx, post)
+
+	return &types.MsgCreatePostResponse{Id: id}, nil
+}
